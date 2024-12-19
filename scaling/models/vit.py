3e523ccf4a299cdd1176dcf5f2cb6877015638b4
Jabareen <nabil.jabareen@charite.de>
2024-12-19 20:31:37 +0100

    Add baseline models and test

---
+++ scaling/models/vit.py
@@ -0,0 +1,365 @@
+from functools import wraps
+from typing import Optional
+
+import torch
+import torch.nn.functional as F
+from einops import rearrange, repeat
+from einops.layers.torch import Rearrange
+from rotary_embedding_torch import RotaryEmbedding
+from torch import einsum, nn
+from torch.nn.attention import SDPBackend, sdpa_kernel
+from torch.nn.functional import scaled_dot_product_attention
+
+
+def exists(val):
+    return val is not None
+
+
+def default(v, d):
+    return v if exists(v) else d
+
+
+def once(fn):
+    called = False
+
+    @wraps(fn)
+    def inner(x):
+        nonlocal called
+        if called:
+            return
+        called = True
+        return fn(x)
+
+    return inner
+
+
+print_once = once(print)
+
+
+def posemb_sincos_1d(patches, temperature=10000, dtype=torch.float32):
+    _, n, dim, device, dtype = *patches.shape, patches.device, patches.dtype
+
+    n = torch.arange(n, device=device)
+    assert (dim % 2) == 0, "feature dimension must be multiple of 2 for sincos emb"
+    omega = torch.arange(dim // 2, device=device) / (dim // 2 - 1)
+    omega = 1.0 / (temperature**omega)
+
+    n = n.flatten()[:, None] * omega[None, :]
+    pe = torch.cat((n.sin(), n.cos()), dim=1)
+    return pe.type(dtype)
+
+
+class FeedForward(nn.Module):
+    def __init__(self, dim, mult=4, dropout=0.0):
+        super().__init__()
+        dim_inner = int(dim * mult)
+        self.net = nn.Sequential(
+            RMSNorm(dim),
+            nn.Linear(dim, dim_inner),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(dim_inner, dim),
+            nn.Dropout(dropout),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+class RMSNorm(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.scale = dim**0.5
+        self.gamma = nn.Parameter(torch.ones(dim))
+
+    def forward(self, x):
+        return F.normalize(x, dim=-1) * self.scale * self.gamma
+
+
+class Attend(nn.Module):
+    def __init__(self, dropout=0.0, flash=False, scale=None):
+        super().__init__()
+        self.scale = scale
+        self.dropout = dropout
+        self.attn_dropout = nn.Dropout(dropout)
+
+        self.flash = flash
+        if not flash:
+            return
+
+        self.use_flash = False
+        if not torch.cuda.is_available():
+            print_once("No GPU detected, using math or mem efficient attention")
+            return
+
+        device_properties = torch.cuda.get_device_properties(torch.device("cuda"))
+
+        if device_properties.major == 8 and device_properties.minor == 0:
+            print_once(
+                "A100 GPU detected, using flash attention if input tensor is on cuda"
+            )
+            self.use_flash = True
+        else:
+            print_once(
+                "Non-A100 GPU detected, using math or mem efficient attention if input tensor is on cuda"
+            )
+
+    def flash_attn(self, q, k, v):
+        if exists(self.scale):
+            default_scale = q.shape[-1] ** -0.5
+            q = q * (self.scale / default_scale)
+
+        if self.use_flash:
+            with sdpa_kernel(SDPBackend.FLASH_ATTENTION):
+                return scaled_dot_product_attention(
+                    q, k, v, dropout_p=self.dropout if self.training else 0.0
+                )
+        else:
+            with sdpa_kernel([SDPBackend.MATH, SDPBackend.EFFICIENT_ATTENTION]):
+                return scaled_dot_product_attention(
+                    q, k, v, dropout_p=self.dropout if self.training else 0.0
+                )
+
+        return out
+
+    def forward(self, q, k, v):
+        """
+        einstein notation
+        b - batch
+        h - heads
+        n, i, j - sequence length (base sequence length, source, target)
+        d - feature dimension
+        """
+
+        q_len, k_len, device = q.shape[-2], k.shape[-2], q.device
+
+        scale = default(self.scale, q.shape[-1] ** -0.5)
+
+        if self.flash:
+            return self.flash_attn(q, k, v)
+
+        # similarity
+
+        sim = einsum(f"b h i d, b h j d -> b h i j", q, k) * scale
+
+        # attention
+
+        attn = sim.softmax(dim=-1)
+        attn = self.attn_dropout(attn)
+
+        # aggregate values
+
+        out = einsum(f"b h i j, b h j d -> b h i d", attn, v)
+
+        return out
+
+
+class Attention(nn.Module):
+    def __init__(
+        self, dim, heads=8, dim_head=64, dropout=0.0, rotary_embed=None, flash=True
+    ):
+        super().__init__()
+        self.heads = heads
+        self.scale = dim_head**-0.5
+        dim_inner = heads * dim_head
+
+        self.rotary_embed = rotary_embed
+
+        self.attend = Attend(flash=flash, dropout=dropout)
+
+        self.norm = RMSNorm(dim)
+        self.to_qkv = nn.Linear(dim, dim_inner * 3, bias=False)
+
+        self.to_gates = nn.Linear(dim, heads)
+
+        self.to_out = nn.Sequential(
+            nn.Linear(dim_inner, dim, bias=False), nn.Dropout(dropout)
+        )
+
+    def forward(self, x):
+        x = self.norm(x)
+
+        q, k, v = rearrange(
+            self.to_qkv(x), "b n (qkv h d) -> qkv b h n d", qkv=3, h=self.heads
+        )
+
+        if exists(self.rotary_embed):
+            q = self.rotary_embed.rotate_queries_or_keys(q)
+            k = self.rotary_embed.rotate_queries_or_keys(k)
+
+        out = self.attend(q, k, v)
+
+        gates = self.to_gates(x)
+        out = out * rearrange(gates, "b n h -> b h n 1").sigmoid()
+
+        out = rearrange(out, "b h n d -> b n (h d)")
+        return self.to_out(out)
+
+
+class Transformer(nn.Module):
+    def __init__(
+        self,
+        dim,
+        depth,
+        heads,
+        dim_head,
+        mlp_mult,
+        rotary_embed: Optional[RotaryEmbedding] = None,
+        flash=True,
+    ):
+        super().__init__()
+        self.norm = nn.LayerNorm(dim)
+        self.layers = nn.ModuleList([])
+        self.rotary_embed = rotary_embed
+        self.flash = flash
+
+        for _ in range(depth):
+            self.layers.append(
+                nn.ModuleList(
+                    [
+                        Attention(
+                            dim,
+                            heads=heads,
+                            dim_head=dim_head,
+                            rotary_embed=self.rotary_embed,
+                            flash=self.flash,
+                        ),
+                        FeedForward(dim, mlp_mult),
+                    ]
+                )
+            )
+
+    def forward(self, x):
+        for attn, ff in self.layers:
+            x = attn(x) + x
+            x = ff(x) + x
+        return self.norm(x)
+
+
+class ECGViT(nn.Module):
+    def __init__(
+        self,
+        *,
+        seq_len,
+        patch_size,
+        num_classes=26,
+        dim=128,
+        depth=2,
+        heads=8,
+        mlp_mult=4,
+        channels=12,
+        dim_head=32,
+        rotary_embed=False,
+        flash=True,
+    ):
+        """
+        Args:
+            seq_len: length of input sequence.
+            patch_size: size of the patch.
+            num_classes: number of classes.
+            dim: dimension of the model.
+            depth: depth of the model.
+            heads: number of heads.
+            mlp_mult: multiplier for the mlp dimension.
+            channels: number of channels.
+            dim_head: dimension of the head.
+            rotary_embed: whether to use rotary embeddings. If False, sinusoidal embeddings are used.
+            flash: whether to use flash attention.
+        """
+        super().__init__()
+
+        assert seq_len % patch_size == 0
+
+        patch_dim = channels * patch_size
+
+        self.rotary_embed = RotaryEmbedding(dim_head) if rotary_embed else None
+        self.cls_token = nn.Parameter(torch.randn(1, 1, dim))
+
+        self.to_patch_embedding = nn.Sequential(
+            Rearrange("b c (n p) -> b n (p c)", p=patch_size),
+            nn.LayerNorm(patch_dim),
+            nn.Linear(patch_dim, dim),
+            nn.LayerNorm(dim),
+        )
+
+        self.transformer = Transformer(
+            dim, depth, heads, dim_head, mlp_mult, self.rotary_embed, flash
+        )
+
+        self.linear_head = nn.Linear(dim, num_classes)
+
+    def forward(self, series):
+        x = self.to_patch_embedding(series)
+
+        # Add class token
+        b, n, _ = x.shape
+        cls_tokens = repeat(self.cls_token, "() n d -> b n d", b=b)
+        x = torch.cat((cls_tokens, x), dim=1)
+
+        # Add positional embedding
+        if not exists(self.rotary_embed):
+            x = x + posemb_sincos_1d(x)
+
+        x = self.transformer(x)
+
+        return self.linear_head(x[:, 0])
+
+
+def vit_tiny(**kwargs) -> ECGViT:
+    seq_len = kwargs.pop("seq_len", 992)
+    patch_size = kwargs.pop("patch_size", 32)
+    return ECGViT(
+        seq_len=seq_len,
+        patch_size=patch_size,
+        dim=256,
+        depth=6,
+        heads=8,
+        mlp_mult=4,
+        dim_head=64,
+        **kwargs,
+    )
+
+
+def vit_small(**kwargs) -> ECGViT:
+    seq_len = kwargs.pop("seq_len", 992)
+    patch_size = kwargs.pop("patch_size", 32)
+    return ECGViT(
+        seq_len=seq_len,
+        patch_size=patch_size,
+        dim=512,
+        depth=8,
+        heads=8,
+        mlp_mult=4,
+        dim_head=64,
+        **kwargs,
+    )
+
+
+def vit_base(**kwargs) -> ECGViT:
+    seq_len = kwargs.pop("seq_len", 992)
+    patch_size = kwargs.pop("patch_size", 32)
+    return ECGViT(
+        seq_len=seq_len,
+        patch_size=patch_size,
+        dim=768,
+        depth=12,
+        heads=12,
+        mlp_mult=4,
+        dim_head=64,
+        **kwargs,
+    )
+
+
+def vit_large(**kwargs) -> ECGViT:
+    seq_len = kwargs.pop("seq_len", 992)
+    patch_size = kwargs.pop("patch_size", 32)
+    return ECGViT(
+        seq_len=seq_len,
+        patch_size=patch_size,
+        dim=1024,
+        depth=24,
+        heads=16,
+        mlp_mult=4,
+        dim_head=64,
+        **kwargs,
+    )
